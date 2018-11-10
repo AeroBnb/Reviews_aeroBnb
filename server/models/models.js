8e7e2c49e9768161796b4a7a0a6d7e0a39eaf624
Inderjeet Kaur <inderjeet.kchawla@gmail.com>
2018-11-09 20:17:02 -0500

    10 Millon recodrs inserted for 2 tables

--- server/models/models.js
+++ server/models/models.js
@@ -2,7 +2,7 @@ const db = require("../../database/postgres.js");
 const Sequelize = require("sequelize");
 
 const User = db.define('user', {
-	id:{
+	id: {
 		type: Sequelize.INTEGER, 
 		autoIncrement: true,
 		primaryKey: true
@@ -23,6 +23,67 @@ const User = db.define('user', {
 		allownull: true,
 		defaultValue: null,
 	},
+	bookings_id: {
+		type: Sequelize.INTEGER,
+		allownull: false,
+	},
+
 });
 
+const Reviews = db.define('reviews', {
+	r_id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		primaryKey: true
+	},
+	listings_id: {
+		type: Sequelize.INTEGER,
+		allownull: false,
+	},
+	user_id: {
+		type: Sequelize.INTEGER,
+		allownull: false,
+	},
+	review_date: {
+		type: Sequelize.DATE,
+	},
+	reviews: {
+		type: Sequelize.TEXT
+	},
+	accuracy: {
+		type: Sequelize.INTEGER
+	},
+	communication: {
+		type: Sequelize.INTEGER
+	},
+	cleanliness: {
+		type: Sequelize.INTEGER
+	},
+	location: {
+		type: Sequelize.INTEGER
+	},
+	check_in: {
+		type: Sequelize.INTEGER
+	},
+	value: {
+		type: Sequelize.INTEGER
+	}
+
+});
+
+
+const Listings = db.define('listings', {
+	l_id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		primaryKey: true
+	},
+	address: {
+		type: Sequelize.STRING,
+	}
+})
+
+
 module.exports.User = User;
+module.exports.Listings = Listings;
+module.exports.Reviews = Reviews;
--- server/models/seed.js
+++ server/models/seed.js
@@ -18,24 +18,7 @@ const getUserData = function() {
   }
   return newUserArray;
 };
-
-const getBookingsData = function() {
-  var newBookingsArray = [];
-  for(let i = 0; i < 1000; i++) {
-    var arr = [];
-    for(let j = 0; j < 1000; j++) {
-      var bookingsData = {
-        stay_start: faker.date.past(),
-        stay_end: faker.date.past(),
-        listings_id: Math.floor(Math.random()* 10000),
-        user_id: Math.floor(Math.random()* 10000),
-      }
-      arr.push(bookingsData)
-    }
-    newBookingsArray.push(arr);
-  }
-  return newBookingsArray;
-};
+// 
 
 const getReviewsData = function() {
   var newReviewsArray = [];
@@ -45,13 +28,15 @@ const getReviewsData = function() {
       var reviewsData = {
         review_date: faker.date.past(),
         review: faker.lorem.paragraph(),
-        accuracy: Math.floor(Math.random()*5),
-        communication: Math.floor(Math.random()*5),
-        cleanliness: Math.floor(Math.random()*5),
-        location: Math.floor(Math.random()*5),
-        check_in: Math.floor(Math.random()*5),
-        value: Math.floor(Math.random()*5),
-        bookings_id: Math.floor(Math.random()*10000),
+        accuracy: Math.floor((Math.random()) *5),
+        communication: Math.floor((Math.random()) *5),
+        cleanliness: Math.floor((Math.random()) *5),
+        location: Math.floor((Math.random()) *5),
+        check_in: Math.floor((Math.random())*5),
+        value: Math.floor((Math.random()) * 5),
+        // address: faker.address.streetAddress(),
+        listings_id: ((Math.random() + 1) * 100),
+        user_id: ((Math.random() + 1) * 100),
       }
       arr.push(reviewsData);
     }
@@ -64,7 +49,7 @@ const getReviewsData = function() {
 var newUserArray = getUserData();
 // console.log(newUserArray);
 // var newBookingsArray = getBookingsData();
-// var newReviewsArray = getReviewsData();
+var newReviewsArray = getReviewsData();
 
-// module.exports = {newUserArray, newBookingsArray, newReviewsArray};
-module.exports = {newUserArray};
+module.exports = {newUserArray, newReviewsArray};
+//module.exports = {newUserArray};
